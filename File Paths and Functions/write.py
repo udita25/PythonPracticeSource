1d4b00dbf480b1b60c592f3fb33ad3dbb2cba6e4
udita <udita2509@gmail.com>
2025-03-24 22:10:31 +0530

    commit

--- File Paths and Functions/write.py
+++ File Paths and Functions/write.py
@@ -3,6 +3,10 @@
 # f.write("This data is written by running a python program.")
 # f.close()
 
-f1 = open('demo2.txt','a') #append mode
-f1.write("\nThis is a new line written by append mode.")
-f1.close()
+# f1 = open('demo2.txt','a') #append mode
+# f1.write("\nThis is a new line written by append mode.")
+# f1.close()
+
+f2 = open('generate.txt', 'w')
+f2.write("This file is generated via code.")
+f2.close()
